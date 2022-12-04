2ea0b7818b7648800917c77c1c08c40666b9f9d0
Hernan Valencia <frjr17@criptext.com>
2022-12-04 09:41:29 -0500

    day4

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
 with open('./puzzle.txt') as file:
-    puzzle =[line.rstrip() for line in file.readlines()]
-    alphabet = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'.split(', ')
-    alphabet_upper = [char.upper() for char in alphabet]
-    alphabet = alphabet+alphabet_upper
-    # print(puzzle)
+    puzzle = [[[int(c.split('-')[0]), int(c.split("-")[1])] for c in line.rstrip().split(",")] for line in file.readlines()]
+
+
+def cr(l):
+    return range(l[0],l[1]+1)
 
 if __name__ == "__main__":
-    results = []
-    for line in puzzle:
-        length = len(line)
-        for letter in line:
-            common = line.find(letter,0,int(length/2)) != -1 and line.find(letter,int(length/2)) != -1
-            if common:
-                results.append(letter)
-                break 
+    results = 0
     
-    results = [alphabet.index(l)+1 for l in results]
-    print('Part One')
-    print('Sum:',sum(results))
+    for group in puzzle:
+        group1 = set(cr(group[0]))        
+        group2 = set(cr(group[1])) 
+        if group1.issuperset(group2) or group2.issuperset(group1):
+            results+=1
 
+    print('Part One')
+    print('Resp:',results)
 # ===================== Part Two ================================
-    results = []
-    i = 0
+    results=0
+    for group in puzzle:
+        group1 = set(cr(group[0]))        
+        group2 = set(cr(group[1])) 
+        if group1.intersection(group2) or group2.intersection(group1):
+            results+=1
+
     
-    while True:
-        if i == len(puzzle):
-            break
-        group = [puzzle[i],puzzle[i+1],puzzle[i+2]]
-       
-        for letter in ''.join(group):
-            common  = letter if group[0].find(letter) != -1 and group[1].find(letter) != -1 and group[2].find(letter)!=-1 else False
-            if common:
-                results.append(common)
-                break
-        i+=3
-   
-    results = [alphabet.index(l)+1 for l in results]
     print('\nPart Two')
-    print('Sum:',sum(results))
-    
+    print('Resp:',results)
