7fd084bcb15919cb5633d3018499300f701a1cdc
Hernan Valencia <frjr17@criptext.com>
2022-12-06 18:00:55 -0500

    Formatting

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from collections import Counter
 with open('./puzzle.txt') as file:
     puzzle= file.readlines()[0].strip()
+    
 if __name__ == "__main__":
     result = 0
+    
     for i in range(4,len(puzzle)):
         group = puzzle[i-4:i]
         if set(Counter(group).values()) =={1}:
             result = i
             break
+
+    # Printing Results
     print('\nPart One')
     print('Resp:', result)
+
 # ===================== Part Two ================================
+    
     for i in range(14,len(puzzle)):
         group = puzzle[i-14:i]
         if set(Counter(group).values()) =={1}:
             result = i
             break
+    
+    # Printing Results
     print('\nPart Two')
     print('Resp:', result)
